01f27b8d4961f9c5bc353d7754ec0b5c1a7d2b6f
RamsesSzobotka <vlastoskr@gmail.com>
2025-07-17 23:54:31 -0500

    fix: Solucionar botones muy pegados.
    -cambiar sentencia sql para que busque editores y no publicadores a la hora de filtrar noticias

--- class/C_noticia.php
+++ class/C_noticia.php
@@ -69,7 +69,7 @@ class Noticia
       $selectFields = "n.*, u.nombre AS nombre_usuario, u.apellido AS apellido_usuario";
       $fromTables = "noticias n 
                        JOIN usuarios u ON n.usuario_id = u.id 
-                       AND u.rol = 'publicador'";
+                       AND u.rol = 'editor'";
 
       if ($categoria === 'todas') {
         $data = $this->db->selectRaw($fromTables, $selectFields, "1 ORDER BY n.id DESC");
